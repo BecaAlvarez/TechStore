6533199fa5b082f39235f16ddb3f2882cf1da2c4
Rebeca Alvarez <63008311+BecaAlvarez@users.noreply.github.com>
2023-07-27 09:55:49 -0300

    :sparkles: Provider

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,24 +2,30 @@ import React, { useState, useEffect } from 'react';
 import fetchProducts from '../../api/fetchProducts';
 import './Products.css';
 import ProductCard from '../ProductCard/ProductCard';
+import Loading from '../Loading/Loading';
 
 function Products(){
   const[products, setProducts] = useState([]);
+  const[loading, setLoading] = useState(true);
 
   useEffect(() =>{
     fetchProducts('iphone').then((response) => {
       setProducts(response);
+      setLoading(false);
     });
   }, []);
 
 
   return(
-    <section className="products contanier">
-      {
-        products.map((product) => <ProductCard key={product.id} data={product}/>)
-      }
-    </section>
-  );
+
+    (
+      loading ? <Loading/> : 
+        <section className="products contanier">
+          {
+            products.map((product) => <ProductCard key={product.id} data={product}/>)
+          }
+        </section>
+    ));
 }
 
 export default Products;
